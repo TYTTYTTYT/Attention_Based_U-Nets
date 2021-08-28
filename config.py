1a2295bb4a58f4e802e8912b2279232a1e076a11
TYTTYTTYT <tai.yintao@gmail.com>
2021-08-28 18:27:23 +0800

    Updated the config file, user can specify data directory now

--- config.py
+++ config.py
@@ -1,1 +1,5 @@
-DATA_SET = 'BrainMRI/data.bi'
+# The home directory to store the Brain MRI dataset.
+DATA_HOME = 'BrainMRI'
+
+# The file path of the pickled numpy array dataset.
+DATA_SET = f'{DATA_HOME}/data.bi'
